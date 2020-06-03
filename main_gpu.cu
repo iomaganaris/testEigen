79d1f34502702af9f2c882ae2f54367c4f344f5c
Ioannis Magkanaris <ioannis.magkanaris@epfl.ch>
2020-06-03 18:36:58 +0200

    Use constant number for the instantiation of the matrices since this is what is done with NMODL
    - Pass Eigen matrices to GPU Kernel

--- main_gpu.cu
+++ main_gpu.cu
@@ -3,28 +3,30 @@
 #include <cstdio>
 
 using Eigen::MatrixXd;
+using Eigen::Matrix;
 using Eigen::VectorXd;
+using Eigen::Vector4d;
 using namespace std;
 
-#define EIGEN_USE_GPU
+//#define EIGEN_USE_GPU
 
 __global__
-void runPartialPivLuGPU(double* m, double* v, double* s, int n){
+void runPartialPivLuGPU(Matrix<double, 4, 4>& m, Vector4d& v, Vector4d& s, int n){
     int index = threadIdx.x + blockIdx.x * blockDim.x;
     int stride = blockDim.x * gridDim.x;
     printf("v in device\n");
     for (int i = index; i < n; i += stride) {
-        printf("v[%d] = %lf\n", i, v[i]);
+        printf("v[%d] = %lf\n", i, v(i));
     }
-    typedef Eigen::Map<Eigen::MatrixXd> MapperMatrix;
+    /*typedef Eigen::Map<Eigen::MatrixXd> MapperMatrix;
     MapperMatrix m_(m, n, n);
     typedef Eigen::Map<Eigen::VectorXd> MapperVector;
-    MapperVector v_(v, n);
-    VectorXd s_ = m_.partialPivLu().solve(v_);
-    s = s_.data();
+    MapperVector v_(v, n);*/
+    s = m.partialPivLu().solve(v);
+    //s = s_.data();
     printf("s in device\n");
     for (int i = index; i < n; i += stride) {
-        printf("%lf\n", s[i]);
+        printf("s[%d] = %lf\n", i, s(i));
     }
 }
 
@@ -41,57 +43,35 @@ int main()
     int threadsPerBlock = 256;
     int numberOfBlocks = 32 * numberOfSMs;
 
-    int n;
+    const int n = 4;
 
-    cout << "Size of the matrix? " << endl;
-    cin >> n;
+    //cout << "Size of the matrix? " << endl;
+    //cin >> n;
 
     //MatrixXd m = MatrixXd::Random(n, n);
     //VectorXd v = VectorXd::Random(n);
-    MatrixXd m(n,n);
-    m << VectorXd::Zero(n), MatrixXd::Identity(n, n).block(0, 0, n, n-1);
-    m = m + MatrixXd::Identity(n, n);
-    VectorXd v(n);
+    Matrix<double, 4, 4> *m;
+    cudaMallocManaged((void**)&m, sizeof(Matrix<double, 4, 4>));
+    *m << VectorXd::Zero(n), MatrixXd::Identity(n, n).block(0, 0, n, n-1);
+    *m = *m + MatrixXd::Identity(n, n);
+    Vector4d *v;
+    cudaMallocManaged((void**)&v, sizeof(Vector4d));
     for (int ii = 0; ii < n-1; ++ii) {
-        v[ii] = 2*ii + 1;
+        (*v)[ii] = 2*ii + 1;
     }
-    v[n-1] = n-1;
+    (*v)[n-1] = n-1;
 
-    VectorXd s = m.partialPivLu().solve(v);
+    Vector4d s = (*m).partialPivLu().solve(*v);
 
-    //MatrixXd m_device = MatrixXd::Random(n, n);
-    //VectorXd v_device = VectorXd::Random(n);
-    double *m_device, *v_device, *s_device;
-    cudaMallocManaged(&m_device, n*n*sizeof(double));
-    cudaMallocManaged(&v_device, n*sizeof(double));
-    cudaMallocManaged(&s_device, n*sizeof(double));
-    VectorXd s2 = VectorXd::Random(n);
-    //m_device = m.data();
-    //v_device = v.data();
-    /*cout << "v_device data:" << endl;
-    for(int i = 0; i < n; ++i) {
-        cout << v_device[i] << endl;
-    }*/
-    //s_device = s2.data();
-    for(int i = 0; i < n*n; ++i) {
-        m_device[i] = m.data()[i];
-    }
-    for(int i = 0; i < n; ++i) {
-        v_device[i] = v.data()[i];
-        s_device[i] = s2.data()[i];
-    }
-
-    cout << "v_device data:" << endl;
-    for(int i = 0; i < n; ++i) {
-        cout << v_device[i] << endl;
-    }
+    Vector4d *s_device;
+    cudaMallocManaged((void**)&s_device, sizeof(Vector4d));
 
     cudaError_t runPartialPivLuGPUError = cudaGetLastError();
     if(runPartialPivLuGPUError != cudaSuccess) {
         cout << "Error after MallocManaged: " << cudaGetErrorString(runPartialPivLuGPUError) << endl;
     }
 
-    runPartialPivLuGPU<<<1, 1>>>(m_device, v_device, s_device, n);
+    runPartialPivLuGPU<<<1, 1>>>(*m, *v, *s_device, n);
 
     runPartialPivLuGPUError = cudaGetLastError();
     if(runPartialPivLuGPUError != cudaSuccess) {
@@ -104,13 +84,13 @@ int main()
     }
 
     cout << "Random matrix:\n\n";
-    cout << m << endl << endl;
+    cout << *m << endl << endl;
     cout << "Random vector:\n\n";
-    cout << v << endl << endl;
+    cout << *v << endl << endl;
     cout << "Solution (x) of M*x = v:\n\n";
     cout << s << endl << endl;
     cout << "Device Solution (x) of M*x = v:\n\n";
-    cout << s_device << endl;
+    cout << *s_device << endl;
 
     cudaFree((void*)&m);
     cudaFree((void*)&v);
